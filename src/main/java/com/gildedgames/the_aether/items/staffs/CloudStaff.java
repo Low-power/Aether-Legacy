70062350dd3c7a7c91dbf2cd0bdd6e21ae43000a
WHR <msl0000023508@gmail.com>
2020-12-29 23:40:51 +0800

    Manually import and refactor commit 7af09ff by bconlon: Shift right clicking the Cloud Staff will clear Cloud Minions. Note: This can become wasteful of durability if used too much since whenever you activate it again it'll use up a durability point (as is how the staff is designed to work).

--- src/main/java/com/gildedgames/the_aether/items/staffs/CloudStaff.java
+++ src/main/java/com/gildedgames/the_aether/items/staffs/CloudStaff.java
@@ -5,6 +5,7 @@ import com.gildedgames.the_aether.items.AetherItems;
 import com.gildedgames.the_aether.player.PlayerAether;
 import com.gildedgames.the_aether.registry.AetherCreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.Entity;
 import net.minecraft.item.EnumRarity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -13,10 +14,10 @@ import net.minecraft.world.World;
 public class CloudStaff extends Item {
 
 	public CloudStaff() {
-		this.setFull3D();
-		this.setMaxDamage(60);
-		this.setMaxStackSize(1);
-		this.setCreativeTab(AetherCreativeTabs.misc);
+		setFull3D();
+		setMaxDamage(60);
+		setMaxStackSize(1);
+		setCreativeTab(AetherCreativeTabs.misc);
 	}
 
 	@Override
@@ -26,23 +27,25 @@ public class CloudStaff extends Item {
 
 	@Override
 	public ItemStack onItemRightClick(ItemStack stack, World world, EntityPlayer player) {
-		PlayerAether playerAether = PlayerAether.get(player);
-
 		if (world.isRemote) {
 			return super.onItemRightClick(stack, world, player);
 		}
 
-		if (playerAether.clouds.isEmpty()) {
+		PlayerAether player_info = PlayerAether.get(player);
+		if (player_info.clouds.isEmpty()) {
 			EntityMiniCloud leftCloud = new EntityMiniCloud(world, player, 0);
 			EntityMiniCloud rightCloud = new EntityMiniCloud(world, player, 1);
-
-			playerAether.clouds.add(leftCloud);
-			playerAether.clouds.add(rightCloud);
-
+			player_info.clouds.add(leftCloud);
+			player_info.clouds.add(rightCloud);
 			world.spawnEntityInWorld(leftCloud);
 			world.spawnEntityInWorld(rightCloud);
-
 			stack.damageItem(1, player);
+		} else if(player.isSneaking()) {
+			for(Entity cloud : player_info.clouds) {
+				if(cloud instanceof EntityMiniCloud) {
+					((EntityMiniCloud)cloud).lifeSpan = 0;
+				}
+			}
 		}
 
 		return stack;
